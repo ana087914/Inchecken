858383edb145237e82bac7dd84d92d570c0ebfc5
simplyana08 <ana.t08@yahoo.com>
2025-03-24 10:52:01 +0100

    Check-in methode aangeroepen in Main

--- src/Inchecken/Main.java
+++ src/Inchecken/Main.java
@@ -2,10 +2,10 @@ package Inchecken;
 
 public class Main {
 	    public static void main(String[] args) {
-	    	double saldo = 0;
+	    	double saldo;
 	    	
 	        Ovchipkaart mijnKaart = new Ovchipkaart(20.00);
-	        Incheckpaal paal = new Incheckpaal(saldo);
-
+	        Incheckpaal paal = new Incheckpaal();
+	        paal.checkIn(mijnKaart);
 	    }
 }
